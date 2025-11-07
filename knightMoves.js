0491dfee78bfb7b93bc18ca3600c7dda4ec36758
Ignas <15948796+ignasc@users.noreply.github.com>
2025-11-07 13:37:46 +0000

    Add comments to functions.

--- knightMoves.js
+++ knightMoves.js
@@ -3,12 +3,14 @@ import knightPiece from "./classKnight.js";
 const visitedPositions = [];
 const boardSize = 8;
 
+// Function to check if two positions are matching.
 function arePositionsSame(pos1, pos2){
     return (
         pos1[0] === pos2[0] && pos1[1] === pos2[1]
     );
 };
 
+// Function to filter out positions that have been visited already.
 function removeVisitedPositions(arrayOfPositions){
     let filteredPositions = arrayOfPositions.filter((position)=>{
         for (let i = 0; i < visitedPositions.length; i++) {
@@ -22,6 +24,7 @@ function removeVisitedPositions(arrayOfPositions){
     return filteredPositions;
 };
 
+// Function that checks if given position is in the given array of positions
 function hasPositionBeenVisited(position, visitedPosArray){
     for (let i = 0; i < visitedPosArray.length; i++) {
         const element = visitedPosArray[i];
@@ -30,6 +33,7 @@ function hasPositionBeenVisited(position, visitedPosArray){
     return false;
 };
 
+// Function that returns the shortest path from starting to end positions and number of moves.
 function knightMoves(startPos, endPos){
 
     if(arePositionsSame(startPos, endPos)){
@@ -55,6 +59,7 @@ function knightMoves(startPos, endPos){
         console.log(`Current level positions to check:`)
         console.log(currentLevelPositions)
 
+        // check all positions at current level if they match endPos. If not, generate next level positions.
         while(currentLevelPositions.length != 0){
             const nextPosition = currentLevelPositions.shift();
 
