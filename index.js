b267a46362766b655f60d2131fb4355a76082933
Ignas <15948796+ignasc@users.noreply.github.com>
2025-11-07 13:38:25 +0000

    Remove unused code.

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-import chessBoard from "./classChessBoard.js"
 import knightPiece from "./classKnight.js";
 import knightMoves from "./knightMoves.js";
 
-console.log("script executed")
-
-const board = new chessBoard(8);
 const knight = new knightPiece([2, 4]);
 
-console.log(board.getBoard());
 console.log(knight.getCurrentPos());
 console.log(knight.getLegalMoves(8));
 
