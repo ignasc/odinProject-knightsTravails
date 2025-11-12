304eaf69c270cbfc7d282879ec483b693cdf13f8
Ignas <15948796+ignasc@users.noreply.github.com>
2025-11-12 13:21:08 +0000

    Remove unused code.

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import knightPiece from "./classKnight.js";
 import knightMoves from "./knightMoves.js";
-
-const knight = new knightPiece([2, 4]);
-
-// console.log(knight.getCurrentPos());
-// console.log(knight.getLegalMoves(8));
 
 const startPosition = [0,0]; //0,0 to 7,7 for 6 moves
 const endPosition = [7,7];
